0c1af83c9ef4f121e19c14c02dc7e5c9df834e3e
pheliperost <phelipe_rost@hotmail.com>
2021-07-11 13:35:19 -0300

    primeira tentativa drawer

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import registerRootComponent from 'expo/build/launch/registerRootComponent';
 
-import App from './src/App';
-//import Menu from './src/Menu';
+//import App from './src/App';
+import Menu from './src/Menu';
 
-registerRootComponent(App);
+registerRootComponent(Menu);
