9207244dccb15717a90cfbf8bb89dd860c5e8968
pheliperost <phelipe_rost@hotmail.com>
2021-07-11 16:15:09 -0300

    Add contador

--- src/Menu.js
+++ src/Menu.js
@@ -2,37 +2,38 @@ import React from 'react';
 import {View,Text, StyleSheet} from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import {createDrawerNavigator} from  '@react-navigation/drawer';
-//import {createDrawerNavigator} from  'react-navigation';
+import { Dimensions } from 'react-native';
 
 import Simples from './componentes/Simples';
 import ParImpar from './componentes/ParImpar';
 import  Inverter  from './componentes/Multi';
-import { Dimensions } from 'react-native';
+import Contador from './componentes/Contador';
 
 const Drawer = createDrawerNavigator();
 
-/* 
-<Drawer.Screen 
-drawerStyle={{marginTop:120}} 
-name="Inverter"
-options={{ drawerLabel: 'Inversao' }}  
- >
-  {props =>(
-    <Inverter texto='sonabuceta' />
-  )}
-</Drawer.Screen> */
+ 
 
 function MyDrawer() {
   return (
     <Drawer.Navigator drawerStyle={{width:250}}>
+        <Drawer.Screen 
+          drawerStyle={{marginTop:250}} 
+          name="Contador"
+          >
+          {props =>(
+            <Contador />
+          )}
+      </Drawer.Screen>
+
       <Drawer.Screen 
-        drawerStyle={{marginTop:120}} 
+        drawerStyle={{marginTop:150}} 
         name="Simples"
         >
         {props =>(
           <Simples texto='ate' />
         )}
       </Drawer.Screen>
+
       <Drawer.Screen 
         name="ParImpar"        
         options={{ drawerLabel: 'Par ou impar' }}
